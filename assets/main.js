f8a6fac1bc611a1e99d7f1b828759ace58f6f546
Criveros <riveroscarlos97@gmail.com>
2022-08-30 13:45:11 -0300

    Updates

---
+++ assets/main.js
@@ -0,0 +1,44 @@
+const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLRe9ARNnYSY6HoUQymwonEd5XfAteiYSU&part=snippet&maxResults=9';
+
+const content = null || document.getElementById('content')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c3bceb8cc6msh98ca06bc3b1c34ap168f22jsnc93e7d9450c5',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options)
+    const data = await response.json()
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API)
+        let view = `
+        ${videos.items.map(video => `
+        <a href="https://youtube.com/watch?v=${video.resourceId}"target="_blank">
+        <div class="group relative">
+            <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+            </h3>
+            </div>
+        </div>
+        `).slice(0, 4).join('')}        
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        alert('La API no esta andando, F')
+        console.error(error)
+    }
+})();
